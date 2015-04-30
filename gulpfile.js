e8a11e71b2a6a2f0551224c99e5c2e5dd6e957a0
John Cheesman <info@johncheesman.org.uk>
2015-04-30 07:42:33 +0100

    Add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp,
     autoprefixer,
     minifycss,
     del,
+    browsersync,
     config;
 
 gulp         = require('gulp');
@@ -10,28 +11,40 @@ sass         = require('gulp-ruby-sass');
 autoprefixer = require('gulp-autoprefixer');
 minifycss    = require('gulp-minify-css');
 del          = require('del');
+browsersync  = require('browser-sync');
 
 config = {
-    src: 'src/scss',
-    dest: 'dist/css',
-    autoprefixer: [
-        'last 2 versions'
-    ]       
+    sass: {
+        src: 'src/scss',
+        dest: 'dist/css',
+        autoprefixer: [
+            'last 2 versions'
+        ]       
+    },
+    serve: {
+        server: {
+            baseDir: 'dist'
+        }
+    }
 };
 
 gulp.task('sass', function() {
-    return sass(config.src)
+    return sass(config.sass.src)
         .pipe(autoprefixer(config.autoprefixer))
         .pipe(minifycss())
-        .pipe(gulp.dest(config.dest));
+        .pipe(gulp.dest(config.sass.dest));
 });
 
 gulp.task('clean', function() {
-    return del(config.dest);
+    return del(config.sass.dest);
+});
+
+gulp.task('serve', function() {
+    return browsersync.init(config.serve);
 });
 
 gulp.task('watch', function() {
     return gulp.watch(config.src, ['sass']);
 });
 
-gulp.task('default', ['clean', 'sass', 'watch']);
+gulp.task('default', ['clean', 'sass', 'serve', 'watch']);
